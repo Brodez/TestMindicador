8e67b66ee73feba7af21a64dc9b8958b07f0f2cb
BrandonH <brandon.h.cofre@gmail.com>
2020-09-04 10:29:59 -0400

    Layouts

---
+++ app/Views/layouts/navbar.php
@@ -0,0 +1,14 @@
+<ul class="sidebar navbar-nav">
+	<li class="nav-item active">
+		<a class="nav-link" href="<?=route_to('main')?>">
+			<i class="fas fa-fw fa-tachometer-alt"></i>
+			<span>Mi Indicador</span>
+		</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" href="<?=route_to('historico-uf')?>">
+			<i class="fas fa-fw fa-chart-area"></i>
+			<span>Historico UF</span></a
+		>
+	</li>
+</ul>
