11d7073e615ec10bf296f501ff5150410c546c35
BrandonH <brandon.h.cofre@gmail.com>
2020-09-04 10:30:11 -0400

    CSS + JS

---
+++ public/js/indicadores.js
@@ -0,0 +1,10 @@
+// $.ajax({
+// 	type: 'POST', //post method
+// 	url: 'url', //ajaxformexample url
+// 	dataType: "json",
+// 	success: function (result, textStatus, jqXHR) {
+// 		chart.data = result;
+// 		chart.update();
+// 	}
+// });
+
