3edf675f2b3b00ec90ab4411d37ce3f3f0f55512
BrandonH <brandon.h.cofre@gmail.com>
2020-09-04 10:40:35 -0400

    MainController
    Controlador Principal que se usara para la prueba

---
+++ app/Controllers/MainController.php
@@ -0,0 +1,31 @@
+<?php namespace App\Controllers;
+
+use App\Models\UFModel;
+
+class MainController extends BaseController
+{
+  public function index()
+  {
+    helper('html');
+
+    echo view('miIndicador/index', ['title' => 'Principal']);
+  }
+
+  public function index_uf()
+  {
+    $UFModel = new UFModel();
+    $UF = $UFModel->findAll();
+    dd($UF);
+    helper('html');
+    echo view('miIndicador/UF/index', ['title' => 'Historico UF']);
+  }
+
+  public function add()
+  {
+    helper('html');
+    echo view('miIndicador/UF/index', ['title' => 'Historico UF']);
+  }
+
+  //--------------------------------------------------------------------
+
+}
